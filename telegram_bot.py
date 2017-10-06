cd3b6e88b8793bcacffcf628a7ea4e935f92b521
Alexander Pitkin <peleccom@gmail.com>
2017-10-06 23:32:28 +0300

    Update

--- telegram_bot.py
+++ telegram_bot.py
@@ -99,6 +99,12 @@ def audio(bot, update):
             logger.debug("Cannot delete tmp file {}".format(file_name))
 
 
+@restricted
+def measure_temp(bot, update):
+    out = subprocess.check_output(["/opt/vc/bin/vcgencmd", "measure_temp"])
+    update.message.reply_text(out)
+
+
 def error(bot, update, error):
     logger.warning('Update "%s" caused error "%s"' % (update, error))
 
@@ -116,6 +122,7 @@ def main():
     dp.add_handler(CommandHandler("photo", photo))
     dp.add_handler(CommandHandler("audio", audio))
     dp.add_handler(CommandHandler("video", video))
+    dp.add_handler(CommandHandler("temp", measure_temp))
     # log all errors
     dp.add_error_handler(error)
 
