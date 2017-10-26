c944afa2fa53dc597376e35877057d764167316a
Alexander Pitkin <peleccom@gmail.com>
2017-10-26 18:29:40 +0300

    Added conversation handler

--- plugins/tts.py
+++ plugins/tts.py
@@ -3,12 +3,16 @@ import logging
 
 import os
 
-from telegram.ext import CommandHandler
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import CommandHandler, ConversationHandler, MessageHandler, Filters, RegexHandler
 
 from plugins.plugin import BasePlugin
 from utils import restricted
 
 logger = logging.getLogger(__name__)
+
+TEXT = 'tts_text'
+END = 'tts_end'
 
 
 class TTSPlugin(BasePlugin):
@@ -20,5 +24,40 @@ class TTSPlugin(BasePlugin):
         subprocess.Popen(["festival", "--tts", "--language", "russian"], stdin=subprocess.PIPE).communicate(
             text.encode('utf8'))
 
+    def tts_text(self, bot, update):
+        """Say text using festival ttl module"""
+        # text = update.message.text[5:]
+        # subprocess.Popen(["festival", "--tts", "--language", "russian"], stdin=subprocess.PIPE).communicate(
+        #     text.encode('utf8'))
+        update.message.reply_text('converting')
+        return TEXT
+
+    def tts_start(self, bot, update):
+        """Say text using festival ttl module"""
+        reply_keyboard = [['Done']]
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+        update.message.reply_text('start',  reply_markup=markup)
+        return TEXT
+
+    def tts_done(self, bot, update):
+        """Say text using festival ttl module"""
+        update.message.reply_text('done')
+        return ConversationHandler.END
+
     def config(self, dp):
         self.add_command_handler(dp, 'tts')
+
+        conv_handler = ConversationHandler(
+            entry_points=[CommandHandler('tts_start', self.tts_start)],
+
+            states={
+                TEXT: [MessageHandler(Filters.text,
+                                      self.tts_text,
+                                      pass_user_data=False),
+                       ]
+            },
+
+            fallbacks=[RegexHandler('^Done$', self.tts_done, pass_user_data=False)]
+        )
+
+        dp.add_handler(conv_handler)
--- telegram_bot.py
+++ telegram_bot.py
@@ -21,7 +21,7 @@ CONFIG_FILENAME = 'config.txt'
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+                    level=logging.DEBUG)
 
 logger = logging.getLogger(__name__)
 
